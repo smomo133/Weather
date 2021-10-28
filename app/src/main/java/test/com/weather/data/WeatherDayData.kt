b11a6e392788a753ae6113e69ed748e75943ea28
DESKTOP-NMQFNTI\YOUN <smomo133@naver.com>
2021-10-28 20:39:40 +0900

    Modify weather day data

--- app/src/main/java/test/com/weather/data/WeatherDayData.kt
+++ app/src/main/java/test/com/weather/data/WeatherDayData.kt
@@ -1,14 +1,16 @@
 package test.com.weather.data
 
+import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
 
+@JsonAdapter(DayDeserializer::class)
 data class WeatherDayData(
     //Maximum temperature in celsius for the day.
-    @field:SerializedName("maxtemp_c") val maxtempC:Float,
+    @field:SerializedName("maxtemp_c") val maxtempC:String,
     //Minimum temperature in celsius for the day
-    @field:SerializedName("mintemp_c") val mintempC:Float,
+    @field:SerializedName("mintemp_c") val mintempC:String,
     //
-    @field:SerializedName("avgtemp_c") val avgtempC:Float,
+    @field:SerializedName("avgtemp_c") val avgtempC:String,
     //
     @field:SerializedName("maxwind_kph") val maxwindKph:Float,
     //Total precipitation in milimeter
@@ -18,7 +20,10 @@ data class WeatherDayData(
     //Average humidity as percentage
     @field:SerializedName("avghumidity") val avghumidity:Float,
     //Weather condition
-    @field:SerializedName("condition") val condition:Condition,
+    //@field:SerializedName("condition") val condition:Condition,
+    @field:SerializedName("condition:text") val conditionText:String,
+    @field:SerializedName("condition:icon") val conditionIconUrl:String,
+    @field:SerializedName("condition:code") val conditionCode:String,
     //UV Index
     @field:SerializedName("uv") val uv:Float,
 )
